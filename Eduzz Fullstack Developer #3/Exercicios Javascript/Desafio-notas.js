80052a817d84f19c1eb95a25bd47c337e80b14e6
Pedro <pedro.rdcampos@hotmail.com>
2022-02-04 23:31:29 -0300

    Adicionando os exercícios e arquivos do bootcamp

---
+++ Eduzz Fullstack Developer #3/Exercicios Javascript/Desafio-notas.js
@@ -0,0 +1,22 @@
+/*Desafio
+Norminia é uma professora e pesquisadora renomada de uma famosa Universidade do Brasil, com inúmeras prêmiações e reconhecimento internacional pelo seu trabalho. Recentemente, Norminia foi convidada para ministrar aulas em uma Universidade da Alemanhã. Mesmo falando muito bem a Língua alemã, Norminia ficou um pouco apreensiva com a responsabilidade, mas resolveu aceitar a proposta e encará-la como um bom desafio.
+Os estudantes adoraram a metodologia de ensino de Norminia e tudo estava indo super bem, até o momento da aplicação da sua primeira prova. Acostumada a dar notas de 0 (zero) a 100 (cem), ela fez o mesmo na primeira prova da sua turma da Alemanhã. No entanto, os estudante acharam estranho, uma vez que na Alemanhã o sistema de notas é diferente: as notas devem ser dadas como conceitos de A a E. O conceito A é o mais alto, enquanto o conceito E é o mais baixo.
+Conversando com outros docentes, ela recebeu a sugestão de utilizar a seguinte tabela, relacionando as notas numéricas com as notas de conceitos:
+*/
+
+let lines = gets().split('\n');
+
+
+let nota = parseInt(lines.shift());
+
+if( nota === 0 ){
+  print("E");
+} else if(nota >= 1 && nota <=35){
+  print("D");
+} else if(nota >=36 && nota <=60){
+  print("C");
+} else if(nota >= 61 && nota <=85){
+  print("B");
+} else if(nota >= 86 && nota <=100){
+  print("A");
+}
---
+++ Eduzz Fullstack Developer #3/README.md
@@ -0,0 +1,15 @@
+# BootCamp EDUZZ FULLSTACK DEVELOPER 3 :heart::heart::heart::heart::heart:
+
+Olá! Bem-vindo!
+
+Alguns dos desafios e arquivos do bootcamp estão nesse repositório:
+
+## Desafios de Javascript
+
+### Exercícios básicos:
+
+* Notas
+* Blobs
+* Cédulas 
+
+### Obrigado !!!
